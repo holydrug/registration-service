a2bf0abbbf05f9850c8360261ab0614ba3fd382f
holydrug <nonprofitxz@gmail.com>
2022-07-10 17:52:22 +0300

    person repository

---
+++ src/main/java/com/popov/registration/service/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.popov.registration.service.repository;
+
+import com.popov.registration.service.entity.person.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+}
