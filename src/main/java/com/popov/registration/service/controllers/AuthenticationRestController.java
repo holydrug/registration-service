822d0815bf12e3e7d90ee45af18da1a83578be1d
holydrug <nonprofitxz@gmail.com>
2022-07-10 17:51:27 +0300

    people controller

---
+++ src/main/java/com/popov/registration/service/controllers/AuthenticationRestController.java
@@ -0,0 +1,58 @@
+package com.popov.registration.service.controllers;
+
+import com.popov.registration.service.entity.person.Person;
+import com.popov.registration.service.repository.PersonRepository;
+import com.popov.registration.service.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationRestController {
+
+    private final AuthenticationManager authenticationManager;
+    private final PersonRepository personRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequestDTO request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            Person person = personRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+            String token = jwtTokenProvider.createToken(request.getEmail(), person.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", request.getEmail());
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
+    }
+
+}
---
+++ src/main/java/com/popov/registration/service/controllers/PeopleController.java
@@ -0,0 +1,48 @@
+package com.popov.registration.service.controllers;
+
+import com.popov.registration.service.entity.Developer;
+import com.popov.registration.service.entity.person.Person;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class PeopleController {
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "AMOGUS", "root"),
+            new Developer(2L, "GERALD", "root"),
+            new Developer(3L, "ISAAC", "root")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('persons:read')")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('persons:write')")
+    public Developer createPerson(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('persons:write')")
+    public void deleteById(@PathVariable Long id) {
+        this.DEVELOPERS.removeIf(person -> person.getId().equals(id));
+    }
+}
