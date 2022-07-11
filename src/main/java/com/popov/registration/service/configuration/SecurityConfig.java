6f282e5fa220f68ee13489c9c8e21192ce9f7581
holydrug <nonprofitxz@gmail.com>
2022-07-11 14:34:38 +0300

    registration section added

--- src/main/java/com/popov/registration/service/configuration/SecurityConfig.java
+++ src/main/java/com/popov/registration/service/configuration/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/v1/auth/login").permitAll()
+                .antMatchers("/api/v1/auth/registration").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
