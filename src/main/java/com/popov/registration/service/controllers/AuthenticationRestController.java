013421669526457f809832b965b74cd7400089cc
holydrug <nonprofitxz@gmail.com>
2022-07-12 15:16:35 +0300

    minor fixes

--- src/main/java/com/popov/registration/service/controllers/AuthenticationRestController.java
+++ src/main/java/com/popov/registration/service/controllers/AuthenticationRestController.java
@@ -1,20 +1,10 @@
 package com.popov.registration.service.controllers;
 
 import com.popov.registration.service.entity.authentication.AuthenticationRequestDTO;
-import com.popov.registration.service.entity.person.Person;
 import com.popov.registration.service.entity.person.dto.PersonDTO;
-import com.popov.registration.service.repository.PersonRepository;
-import com.popov.registration.service.security.JwtTokenProvider;
-import com.popov.registration.service.utils.mappers.person.PersonMapper;
+import com.popov.registration.service.service.authentication.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,51 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationRestController {
 
-    private final AuthenticationManager authenticationManager;
-    private final PersonRepository personRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final PasswordEncoder passwordEncoder;
+private final AuthenticationService authenticationService;
 
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequestDTO request) {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-            Person person = personRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
-            String token = jwtTokenProvider.createToken(request.getEmail(), person.getRole().name());
-            Map<Object, Object> response = new HashMap<>();
-            response.put("email", request.getEmail());
-            response.put("token", token);
-            return ResponseEntity.ok(response);
-        } catch (AuthenticationException e) {
-            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
-        }
+        return authenticationService.authenticate(request);
     }
 
     @PostMapping("/logout")
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
-        securityContextLogoutHandler.logout(request, response, null);
+        authenticationService.logout(request, response);
     }
 
     @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody PersonDTO personDTO) {
-        Person person = PersonMapper.INSTANCE.toPerson(personDTO);
-        String token = jwtTokenProvider.createToken(person.getEmail(), person.getRole().name());
-        Map<Object, Object> response = new HashMap<>();
-        response.put("email", person.getEmail());
-        response.put("token", token);
-        person.setPassword(passwordEncoder.encode(person.getPassword()));
-        personRepository.save(person);
-        return ResponseEntity.ok(response);
+        return authenticationService.registration(personDTO);
     }
 
 }
