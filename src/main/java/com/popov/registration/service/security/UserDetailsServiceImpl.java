5adba9899934c47c7a9f4f3db0b7aeb1d54895d1
holydrug <nonprofitxz@gmail.com>
2022-07-14 12:08:45 +0300

    minor fixes

--- src/main/java/com/popov/registration/service/security/UserDetailsServiceImpl.java
+++ src/main/java/com/popov/registration/service/security/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.popov.registration.service.security;
 
+import com.popov.registration.service.service.authentication.attempt.LoginAttemptService;
 import com.popov.registration.service.entity.person.Person;
+import com.popov.registration.service.entity.person.etc.Status;
 import com.popov.registration.service.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,18 +10,50 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Service("userDetailsServiceImpl")
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final PersonRepository personRepository;
+    private final LoginAttemptService loginAttemptService;
+    private final HttpServletRequest request;
 
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Person person = personRepository.findByEmail(email).orElseThrow(() ->
+        /*Person person = personRepository.findByEmail(email).orElseThrow(() ->
                 new UsernameNotFoundException("User doesn't exists"));
-        return SecurityUser.fromUser(person);
+        return SecurityUser.fromUser(person);*/
+
+        String ip = getClientIP();
+        if (loginAttemptService.isBlocked(ip)) {
+            throw new RuntimeException("blocked");
+        }
+
+        try {
+            Person user = personRepository.findByEmail(email).get();
+            if (user == null) {
+                return new org.springframework.security.core.userdetails.User(
+                        " ", " ", true, true, true, true,
+                        user.getRole().getAuthorites());
+            }
+
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(), user.getPassword(), user.getStatus().equals(Status.ACTIVE), true, true, true,
+                    user.getRole().getAuthorites());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getClientIP() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null) {
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
     }
 }
